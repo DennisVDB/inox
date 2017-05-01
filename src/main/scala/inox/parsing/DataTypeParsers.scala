e0704641754762052b6a6e8fb685c9937559b9cf
starcalm <junzebao@gmail.com>
2017-05-01 10:55:53 +0200

    add skeleton for DataTypeParsers

---
+++ src/main/scala/inox/parsing/DataTypeParsers.scala
@@ -0,0 +1,13 @@
+package inox
+package parsing
+
+/**
+  * Created by junze on 5/1/17.
+  */
+trait DataTypeParsers { self: Interpolator =>
+
+  class DataTypeParser extends TypeParser {
+
+  }
+
+}
