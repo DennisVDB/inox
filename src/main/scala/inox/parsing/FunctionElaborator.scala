87896c265f791dac785a52ee39737199366a458c
Dennis van der Bij <d.vanderbij@gmail.com>
2017-05-22 16:52:36 +0200

    Build symbols incrementally

--- src/main/scala/inox/parsing/FunctionElaborator.scala
+++ src/main/scala/inox/parsing/FunctionElaborator.scala
@@ -9,17 +9,39 @@ trait FunctionElaborators { self: Interpolator =>
 
   class FunctionElaborator { inner: FunctionIR.type =>
 
-    def getFunction(f: Function): trees.FunDef = f match {
-      case Function(id, tParams, args, retType, body) =>
-        val funIdentifier = FreshIdentifier(id.getName)
-        val typeParamsDef = tParams.map(_.getName).map(trees.TypeParameter.fresh).map(trees.TypeParameterDef(_))
-        val paramValDef = args.map {
-          case Arg(id, tpe) => trees.ValDef(FreshIdentifier(id.getName), TypeIR.getType(tpe), Set.empty)
-        }
-        val returnType = TypeIR.getType(retType)
-        val funBody = ExprIR.getExpr(body)
-        new trees.FunDef(funIdentifier, typeParamsDef, paramValDef, returnType, funBody, Set.empty)
+    def getFunctions(fs: List[Function])(
+        symbols: trees.Symbols): trees.Symbols = {
+      fs.foldLeft(symbols)((s, f) => getFunction(f)(s))
     }
+
+    def getFunction(f: Function)(symbols: trees.Symbols): trees.Symbols =
+      symbols.withFunctions(Seq(f match {
+        case Function(id, tParams, args, retType, body) =>
+          val funIdentifier = FreshIdentifier(id.getName)
+
+          val typeParamsDef = tParams
+            .map(_.getName)
+            .map(trees.TypeParameter.fresh)
+            .map(trees.TypeParameterDef(_))
+
+          val paramValDef = args.map {
+            case Arg(id, tpe) =>
+              trees.ValDef(FreshIdentifier(id.getName),
+                           TypeIR.getType(tpe),
+                           Set.empty)
+          }
+
+          val returnType = TypeIR.getType(retType)
+
+          val funBody = ExprIR.getExpr(body)
+
+          new trees.FunDef(funIdentifier,
+                           typeParamsDef,
+                           paramValDef,
+                           returnType,
+                           funBody,
+                           Set.empty)
+      }))
 
   }
 }
